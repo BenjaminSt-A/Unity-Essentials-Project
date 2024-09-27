1c8833d17815f3af8eeed7483f1eb33f606f7a39
BenjaminSt-A <benjamin.st_amand@hotmail.com>
2024-09-26 20:04:06 -0400

    collect effect

--- Assets/_Unity Essentials/Scripts/Collectible.cs
+++ Assets/_Unity Essentials/Scripts/Collectible.cs
@@ -6,6 +6,7 @@ public class Collectible : MonoBehaviour
 { 
 
 public float rotationSpeed;
+public GameObject onCollectEffect;
 
     // Start is called before the first frame update
     void Start()
